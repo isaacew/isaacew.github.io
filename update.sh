c91bc8f98145b1781767e0765fc6e80f0e058164
Alexander Von Moll <avonmoll@gmail.com>
2018-10-31 14:08:00 -0400

    Quick update script

---
+++ update.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+papis export --all --bibtex --out bibliography.bib project=MyPubs
+RC1=$?
+if [[ $RC1 -eq 0 ]]; then
+	markdown_generator/publications.py
+	RC2=$?
+	if [[ $RC2 -eq 0 ]]; then
+		echo "Success"
+	else
+		echo "Markdown generator failed"
+	fi
+else
+	echo "papis export failed"
+fi
+	
